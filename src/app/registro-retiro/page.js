4fbc605128cc1c661a7162503c745e7fda826a65
Emerson Jimenez Loaiza <emersonnicolas.jl@gmail.com>
2025-01-10 02:04:27 -0300

    verificacion

--- src/app/registro-retiro/page.js
+++ src/app/registro-retiro/page.js
@@ -1,4 +1,5 @@
 "use client";
+<<<<<<< HEAD
 import RetirosForm from "../../components/retirosForm/RetirosForm";
 import withAuth from "../../components/withAuth";
 
@@ -6,8 +7,23 @@ function registroRetiro() {
   return (
     <div>
       <RetirosForm />
+=======
+import RetiroForm from "../../components/retiroForm/RetiroForm";
+import withAuth from "../../components/withAuth";
+import { useSessionTimeout } from "@/middleware/auth";
+
+function RegistroRetiro() {
+  useSessionTimeout();
+  return (
+    <div>
+      <RetiroForm />
+>>>>>>> 4f057c0f767374449cd317203d1b32042dff7348
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default withAuth(registroRetiro);
+=======
+export default withAuth(RegistroRetiro);
+>>>>>>> 4f057c0f767374449cd317203d1b32042dff7348
